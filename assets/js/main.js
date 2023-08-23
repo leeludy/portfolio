ee3e95a313b7e6fb6a30235e066fb65b11aed617
sibylle <sibylle.p@protonmail.com>
2023-08-23 16:05:01 +0200

    carousel swipe

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,4 +34,62 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
     scrollToTop();
   };
+
+  /* Swipe */
+  function detectSwipe(id, f) {
+    let detect = {
+        startX: 0,
+        startY: 0,
+        endX: 0,
+        endY: 0,
+        minX: 30,
+        maxY: 60,
+      },
+      direction = null,
+      element = document.getElementById(id);
+
+    element.addEventListener("touchstart", function (event) {
+      const touch = event.touches[0];
+      detect.startX = touch.screenX;
+      detect.startY = touch.screenY;
+    });
+
+    element.addEventListener("touchmove", function (event) {
+      event.preventDefault();
+      const touch = event.touches[0];
+      detect.endX = touch.screenX;
+      detect.endY = touch.screenY;
+    });
+
+    element.addEventListener("touchend", function (event) {
+      if (
+        Math.abs(detect.endX - detect.startX) > detect.minX &&
+        Math.abs(detect.endY - detect.startY) < detect.maxY
+      ) {
+        direction = detect.endX > detect.startX ? "right" : "left";
+      }
+
+      if (direction !== null && typeof f === "function") {
+        f(element, direction);
+      }
+    });
+  }
+
+  function swipeDirection(element, direction) {
+    const inputsCaroussel = element.getElementsByTagName("input");
+
+    for (var i = 0; i < inputsCaroussel.length; i++) {
+      if (inputsCaroussel[i].type == "radio" && inputsCaroussel[i].checked) {
+        if (direction == "right" && inputsCaroussel[i - 1] !== undefined) {
+          inputsCaroussel[i].checked = false;
+          inputsCaroussel[i - 1].checked = "checked";
+        }
+        if (direction == "left" && inputsCaroussel[i + 1] !== undefined) {
+          return (inputsCaroussel[i].checked = false), (inputsCaroussel[i + 1].checked = "checked");
+        }
+      }
+    }
+  }
+
+  detectSwipe("sliderContainer", swipeDirection);
 });
